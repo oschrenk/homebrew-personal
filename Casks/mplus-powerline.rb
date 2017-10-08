95630ceac51df013bf1fe6c5255d0434b964fabf
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-08 13:45:18 +0200

    Initial commit

---
+++ Casks/mplus-powerline.rb
@@ -0,0 +1,10 @@
+cask 'mplusForPowerline-1mn-light' do
+  version '1.0.0'
+  sha256 'bd67022eb2a64e0bbd487aecc077460d23dfce5d2822c54bf2f62e71a192b4ac'
+
+  url "https://github.com/oschrenk/mplus-powerline/releases/downloads/v#{version}/mplusForPowerline-1mn-light.ttf.zip"
+  name 'mplus for powerline'
+  homepage 'https://github.com/oschrenk/mplus-powerline'
+
+  font 'mplusForPowerline-1mn-light.ttf'
+end
