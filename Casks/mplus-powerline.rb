b07f09a57820b865dabe2b9298f05553f68149a8
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-08 13:55:02 +0200

    Fix typos

--- Casks/mplus-powerline.rb
+++ Casks/mplus-powerline.rb
@@ -1,8 +1,8 @@
-cask 'mplusForPowerline-1mn-light' do
+cask 'mplus-powerline' do
   version '1.0.0'
   sha256 'bd67022eb2a64e0bbd487aecc077460d23dfce5d2822c54bf2f62e71a192b4ac'
 
-  url "https://github.com/oschrenk/mplus-powerline/releases/downloads/v#{version}/mplusForPowerline-1mn-light.ttf.zip"
+  url "https://github.com/oschrenk/mplus-powerline/releases/download/v#{version}/mplusForPowerline-1mn-light.ttf.zip"
   name 'mplus for powerline'
   homepage 'https://github.com/oschrenk/mplus-powerline'
 
