f0eb23e52be9b6695ff45d672876db926d2c948e
Oliver Schrenk <oliver.schrenk@gmail.com>
2021-03-20 16:33:04 +0100

    mplus mn nerd font

---
+++ Casks/mplus-mn-nerd-font-complete.rb
@@ -0,0 +1,15 @@
+cask 'mplus-powerline' do
+  version '1.0'
+  sha256 'bd67022eb2a64e0bbd487aecc077460d23dfce5d2822c54bf2f62e71a192b4ac'
+
+  url "https://github.com/oschrenk/mplus-mn-nerd-font-complete/files/6175868/mplus-mn-nerd-font-complete.zip"
+
+  name 'mplus mn nerd font complete'
+  homepage 'https://github.com/oschrenk/mplus-mn-nerd-font-complete'
+
+  font 'M+ 1mn bold Nerd Font Complete.ttf'
+  font 'M+ 1mn light Nerd Font Complete.ttf'
+  font 'M+ 1mn medium Nerd Font Complete.ttf'
+  font 'M+ 1mn Nerd Font Complete.ttf'
+  font 'M+ 1mn thin Nerd Font Complete.ttf'
+end
