cffbfdea6ce235e5ee37853ff89e996b3e190376
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-14 23:41:11 +0200

    chore: Add autoclick

---
+++ Casks/autoclick.rb
@@ -0,0 +1,11 @@
+cask "autoclick" do
+  name "Autoclick"
+  desc "Simulate mouse clicks"
+  homepage "https://github.com/inket/Autoclick"
+
+  version "2.0.5"
+  sha256 "47b2794d5be154109c066200d5a6633536147ca5085016459f00ba9c68dcbc1b"
+  url "https://tars.mahdi.jp/apps/autoclick.zip"
+
+  app "Autoclick.app"
+end
