546a0a77bf64a4a8ea89277ad291678e3470845f
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-06-14 22:45:39 +0200

    icon-composer: latest

--- Casks/icon-composer.rb
+++ Casks/icon-composer.rb
@@ -4,7 +4,8 @@ cask "icon-composer" do
   homepage "https://developer.apple.com/icon-composer"
 
   url "https://developer.apple.com/download/files/icon-composer.dmg"
-  sha256 "11c04756830ad457387f9fff8710e4ba3e0a522c9cef9c6cf597915eb55575e5"
+  version :latest
+  sha256 :no_check
 
   app "Icon Composer.app"
 end
