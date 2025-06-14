bc84b23131520752ac0aa5c9aeec228e1cc233bc
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-06-14 22:37:16 +0200

    cask: icon-composer

---
+++ Casks/icon-composer.rb
@@ -0,0 +1,10 @@
+cask "icon-composer" do
+  name "Icon Composer"
+  desc "Create Icons"
+  homepage "https://developer.apple.com/icon-composer"
+
+  url "https://developer.apple.com/download/files/icon-composer.dmg"
+  sha256 "11c04756830ad457387f9fff8710e4ba3e0a522c9cef9c6cf597915eb55575e5"
+
+  app "Icon Composer.app"
+end
