0284e9e346dfcd1b87cbfe2dccc401f146659157
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-07-15 15:19:17 +0200

    fix version

--- Formula/hullcaster.rb
+++ Formula/hullcaster.rb
@@ -1,7 +1,9 @@
 class Hullcaster < Formula
   desc "Terminal-based podcast manager written in Rust"
   homepage "https://github.com/gilcu3/hullcaster"
-  url "https://github.com/gilcu3/hullcaster.git"
+  version "0.1.2"
+  url "https://github.com/gilcu3/hullcaster/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "cb3242a11333a227b486ca0c4847f83f2db980e5b2334eca5242172e861e22e0"
   license "GPL-3.0"
   head "https://github.com/gilcu3/hullcaster.git", branch: "main"
 
