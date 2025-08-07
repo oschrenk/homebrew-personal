6f2e034fd9b1c1b11a5c88b5dd5d698ce3b3ba81
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-08-07 19:58:22 +0200

    cask: bongocat

---
+++ Casks/bongocat.rb
@@ -0,0 +1,11 @@
+cask "bongocat" do
+  name "BongoCat"
+  desc "Simulate mouse clicks"
+  homepage "https://github.com/Gamma-Software/BongoCat-mac"
+
+  version "1.8.2"
+  sha256 "6cf6eddf827086a8117612e094c8489c86aab24e0d5f33fbf81a8114fa457e98"
+  url "https://github.com/Gamma-Software/BongoCat-mac/releases/download/1.8.2/BongoCat-1.8.2.dmg"
+
+  app "BongoCat.app"
+end
