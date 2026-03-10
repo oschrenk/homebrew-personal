1d11e578db49ee05c76bb5f9212438e76b6c660a
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-03-10 21:12:28 +0100

    chore: Bump dora to 2.0.0

--- Formula/dora.rb
+++ Formula/dora.rb
@@ -2,12 +2,12 @@ class Dora < Formula
   desc "Code context CLI for AI agents"
   homepage "https://github.com/butttons/dora"
   license "MIT"
-  version "1.6.0"
+  version "2.0.0"
 
   on_macos do
     on_arm do
       url "https://github.com/butttons/dora/releases/download/v#{version}/dora-darwin-arm64"
-      sha256 "aaa8f34812a65cd033794cd610901e6cd533b5164069e60ffb55bdfb7304b5af"
+      sha256 "bcfe53e5f355797a770176520bd0339da25bfd9bb0ac1c7b7d6eb5bade2b8af8"
     end
   end
 
