41481f4dfdbe35eb709f2c8f96c54f1a2f6ab179
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-03-25 10:26:08 -0600

    brew: TheTimeMachineMechanic

---
+++ Casks/the-time-machine-mechanic.rb
@@ -0,0 +1,11 @@
+cask "the-time-machine-mechanic" do
+  name "The Time Machine Mechanic"
+  desc "Check and manage Time Machine backups"
+  homepage "https://eclecticlight.co/time-machine-mechanic/"
+
+  version "2.03"
+  sha256 "d1eafe2bb10465822d24b15ed42a5c618364cb2a36816326a63b0babceaa6247"
+  url "https://eclecticlight.co/wp-content/uploads/2025/06/t2m2203.zip"
+
+  app "t2m2203/TheTimeMachineMechanic.app"
+end
