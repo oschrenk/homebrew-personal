1b58dfda8e6ee4874a6d6829dd83d7f7d772a384
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-04-11 08:44:18 -0600

    brew: Cotypist

---
+++ Casks/cotypist.rb
@@ -0,0 +1,11 @@
+cask "cotypist" do
+  name "Cotypist"
+  desc "Typing practice app for macOS"
+  homepage "https://cotypist.app"
+
+  version "0.21"
+  sha256 "008631a2fda080a4adb75ab60eb5a9777d191b58bc8daf40a20480bbc0389d87"
+  url "https://cotypist.app/download/Cotypist.dmg"
+
+  app "Cotypist.app"
+end
