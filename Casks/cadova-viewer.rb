86ba998562741eab9e02c87a85dad3a0f1b564f6
Oliver Schrenk <oliver.schrenk@gmail.com>
2026-06-09 20:58:54 -0600

    cadova-viewer: bump to 0.4.0

--- Casks/cadova-viewer.rb
+++ Casks/cadova-viewer.rb
@@ -1,6 +1,6 @@
 cask "cadova-viewer" do
-  version "0.3.0"
-  sha256 "e0c38512982a2a92edb978dac2c4e7425d814d4acd96d5d3bb2cb35f4ce98707"
+  version "0.4.0"
+  sha256 "a54a5c888f2308636761e814833edf9b9303893567fcf0425aed866b043f6fb2"
 
   url "https://github.com/tomasf/CadovaViewer/releases/download/#{version}/CadovaViewer#{version}.zip"
   name "Cadova Viewer"
